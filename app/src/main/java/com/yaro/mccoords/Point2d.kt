ba9b5a25ad2a953da48a054db4f40ad66582925c
Yaroslav Kish <yaro.kish@gmail.com>
2024-07-01 06:36:07 +0300

    Add Point2d basic operators

--- app/src/main/java/com/yaro/mccoords/Point2d.kt
+++ app/src/main/java/com/yaro/mccoords/Point2d.kt
@@ -1,3 +1,30 @@
 package com.yaro.mccoords
 
+import kotlin.math.sqrt
+
 data class Point2d(val x: Int = 0, val z: Int = 0)
+
+operator fun Point2d.plus(other: Point2d): Point2d {
+    return Point2d(x + other.x, z + other.z)
+}
+
+operator fun Point2d.minus(other: Point2d): Point2d {
+    return Point2d(x - other.x, z - other.z)
+}
+
+operator fun Point2d.times(scalar: Int): Point2d {
+    return Point2d(x * scalar, z * scalar)
+}
+
+infix fun Point2d.dot(other: Point2d): Int {
+    return x * other.x + z * other.z
+}
+
+fun Point2d.magnitude2(): Int {
+    return x * x + z * z
+}
+
+fun Point2d.magnitude(): Double {
+    return sqrt(this.magnitude2().toDouble())
+}
+
